3e592edb679e7767db45b97ae0ada51a968c68d8
veer2238 <114563813+veer2238@users.noreply.github.com>
2023-04-28 11:14:39 +0530

    dcdc

---
+++ scroll_top/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './App.css'
+import ScrollToTop from "react-scroll-to-top";
+
+
+
+const App = () => {
+  return (
+
+
+
+    <div>
+    
+   
+
+    <ScrollToTop smooth />
+    <div className='main1'>
+        Home
+    </div>
+
+    <div className='main2'>
+        contact
+    </div>
+
+    <div className='main3'>
+        service
+    </div>
+
+    <div className='main4'>
+        about
+    </div>
+    
+
+  
+    
+  
+    
+    
+    </div>
+ 
+  )
+}
+
+export default App
